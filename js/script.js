6926d47ec5b7019aee4bf5d4641a72fbac5fc883
AbrahamOdoi <abrahamodoi2002@gmail.com>
2015-04-17 12:02:33 +0000

    1st
    1st Commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).on("pagecreate", function() {
+	$("#splahLabel1").slideDown(3000, function() {
+		// window.location = 'login.php';
+		window.location = 'http://216.224.161.207/wisdom/login.php'; 
+	});
+});
